ad7e4e47a1d8b1b1ddc27660cb12494d3719f1b3
David Bayer <drbayer@eternalstench.com>
2021-07-27 17:01:47 -0700

    Updated stats tracker to post log messages to Slack in addition to log file.

--- track_meds.py
+++ track_meds.py
@@ -5,18 +5,43 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import RPi.GPIO as GPIO
 from time import sleep
+from slack_sdk.webhook import WebhookClient
+import json
 
-
+# Google Config
 credential_file = "blood-pressure-stats-3437e823e42b.json"
 google_sheets_file = "BP Meds Taken"
-gpio_input_pin = 10
-logfile = "bpstatstracker.log"
 
-def log(msg):
+# RPi Config
+gpio_input_pin = 10
+
+# Logging Config
+logfile = "bpstatstracker.log"
+slack_config_file = "slack-config.json"
+
+with open(slack_config_file, "r") as f:
+    slack_config = json.loads(f.read())
+    slack_url = slack_config['logs_url']
+
+
+def log(msg, log_level = "INFO", post_to_slack = True):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    logmessage = "%s %s\n" % (timestamp, msg)
+    logmessage = f"{timestamp} BPStatsTracker {log_level} {msg}\n"
     with open(logfile, "a") as log:
         log.write(logmessage)
+    if post_to_slack:
+        tell_slack(logmessage)
+
+
+def tell_slack(msg):
+    if slack_url == None:
+        log("Slack web hook URL not found!", log_level="ERROR", post_to_slack=False)
+        return
+    slack_logger = WebhookClient(slack_url)
+    response = slack_logger.send(text=msg)
+    logmessage = f"Posting log message to Slack. Response code: {response.status_code}. Response body: {response.body}"
+    loglevel = "INFO" if response.status_code == 200 else "ERROR"
+    log(logmessage, log_level=loglevel, post_to_slack=False)
 
 
 def button_pressed(channel):
@@ -38,13 +63,13 @@ def record_time(channel):
     now = datetime.now().strftime("%H:%M:%S")
 
     sheet.append_row([today, now])
-    log("Wrote timestamp to Google Sheet '%s'" % (google_sheets_file))
+    log(f"Wrote meds taken timestamp to Google Sheet {google_sheets_file}")
 
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(gpio_input_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(gpio_input_pin, GPIO.RISING, callback=record_time, bouncetime=200)
+GPIO.add_event_detect(gpio_input_pin, GPIO.RISING, callback=record_time, bouncetime=500)
 
 log("Waiting for button presses")
 while True:
